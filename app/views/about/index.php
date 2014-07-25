222bb374ec798df683cfc606a5650210432452d1
enegri <enegri@enegri-ThinkPad-Edge>
2014-07-25 18:18:13 -0300

    First commit

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,6 +1,6 @@
 <article>
     <h1>About</h1>
     <p>Hi!. I'm Esteban Negri, the author of Mandala Framework and this website has been developed with it. The website uses a template built with <a href="http://www.initializr.com/" target="_blank">Initializr template generator.</a> 
-        and <a href="http://prismjs.com" target="_blank">Prism JS for syntax highlighting</a>.</p>
-    <p>If you wanna use or extend Mandala Framework, please go ahead and download or fork its GitHub project.</p>
+        and <a href="http://prismjs.com" target="_blank">Prism JS</a> for syntax highlighting.</p>
+    <p>If you wanna use or extend Mandala Framework, please go ahead and <a href="https://github.com/enbits/mandala_framework/archive/master.zip">download</a> or fork its <a href="https://github.com/enbits/mandala_framework">GitHub project</a>.</p>
 </article>
